c8937a91016ddf2c9d65f2ee038c7d0c8a632ef3
agusthas <suzumantan@gmail.com>
2021-04-06 11:10:36 +0700

    changing how to toggle the class

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,7 @@
 const accordionTitle = document.querySelectorAll('.js-accordion__title');
 
 accordionTitle.forEach((acc) => {
-  acc.addEventListener('click', toggleActive);
-});
-
-function toggleActive() {
-  accordionTitle.forEach((acc) => {
-    acc.classList.remove('accordion__title-active');
+  acc.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('accordion__title-active');
   });
-  this.classList.add('accordion__title-active');
-}
+});
